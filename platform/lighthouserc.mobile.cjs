e95e9350e5f9f6676b22d82a589c005c50a9d2b8
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-05-16 21:54:51 +0300

    Make mobile Lighthouse audit fast and stable

--- platform/lighthouserc.mobile.cjs
+++ platform/lighthouserc.mobile.cjs
@@ -2,15 +2,11 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './dist',
-      url: [
-        '/',
-        '/lessons/01-fintech-qa/',
-        '/lessons/05-idempotency/',
-        '/lessons/08-final-project/',
-      ],
+      url: ['/'],
       numberOfRuns: 1,
       settings: {
         throttlingMethod: 'simulate',
+        maxWaitForLoad: 30000,
         chromeFlags: '--no-sandbox --disable-dev-shm-usage',
       },
     },
