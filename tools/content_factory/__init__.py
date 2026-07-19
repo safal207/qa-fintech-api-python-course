2a341ed4c9140afbcbc3b2a1959791760465a6aa
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-07-19 13:00:52 +0300

    feat: scaffold course content factory

---
+++ tools/content_factory/__init__.py
@@ -0,0 +1,5 @@
+"""Deterministic course content-pack generator."""
+
+from .cli import build_content_pack, export_content_pack, parse_module
+
+__all__ = ["build_content_pack", "export_content_pack", "parse_module"]
